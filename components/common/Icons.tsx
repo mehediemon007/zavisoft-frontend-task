68762476b05e01280c5d7d3401ad03c89675a350
mehediemon007 <mehediemon8=7@gmail.com>
2026-02-23 21:18:31 +0600

    update: add caretDownIcon to iconSet

--- components/common/Icons.tsx
+++ components/common/Icons.tsx
@@ -120,3 +120,9 @@ export const BinIcon = ({ className }: IconProps) => (
     </svg>
 
 );
+
+export const CaretDownLineIcon = ({ className }: IconProps) => (
+    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" className={className}>
+        <path d="M5.25 9L12 15.75L18.75 9" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
+    </svg>
+);
